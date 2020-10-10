357d01ada2944d43a0bcedaa4d48c67e01d3a4bd
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2020-10-09 23:30:50 -0600

    fixed file location

--- WindowsFormCalculator/Validator.cs
+++ WindowsFormCalculator/Validator.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsFormCalculator.Properties
+namespace WindowsFormCalculator
 {
     public static class Validator
     {
-        
     }
 }
